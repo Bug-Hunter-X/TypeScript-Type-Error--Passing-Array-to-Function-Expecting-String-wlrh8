0fb630fa4bf28cdb63a5c6a3b566318a0cdd0be5
BugHunter-X <askzenai@gmail.com>
2025-01-04 20:28:04 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,9 @@
+function greeter(person) {
+  return "Hello, " + person;
+}
+
+let user = ["Bob", "Alice"];
+
+console.log(greeter(user)); //outputs Hello,Bob,Alice
+//Expected output: Hello, Bob
+//Expected output: Hello, Alice
